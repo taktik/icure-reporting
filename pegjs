0159edd9136e47a739e452e044a881e0c9f086f3
Simon Hardy <sha@taktik.com>
2019-09-26 14:08:02 +0200

    misc improvements / start working on HE

--- pegjs
+++ pegjs
@@ -1,17 +1,55 @@
 {
 	let hcpId = "782f1bcd-9f3f-408a-af1b-cd9f3f908a98"
-    let svcFilter = (left, right) => {
-    	if (left.indexOf(':') === 0) {
-            return {
-      			'$type':'ServiceByHcPartyTagCodeDateFilter',
-      			'healthcarePartyId':hcpId, 'tagCode':right, 'tagType':left.substr(1)
-    		}
+    let svcFilter = (left, right, rightMost) => {
+        let filter = {
+            '$type':'ServiceByHcPartyTagCodeDateFilter',
+            'healthcarePartyId':hcpId
+        }
+        if (left.indexOf(':') === 0) {
+            filter.tagCode = right
+            filter.tagType = left.substr(1)
         } else {
-             return {
-      			'$type':'ServiceByHcPartyTagCodeDateFilter',
-      			'healthcarePartyId':hcpId, 'codeCode':right, 'codeType':left
-    		}
-       	}
+            filter.codeCode = right
+            filter.codeType = left
+        }
+        if (rightMost && rightMost.length == 2) {
+            let startDate = rightMost[0]
+            if (startDate.length > 0) {
+                if (startDate.length <= 8) startDate += "000000"
+                filter.startValueDate = startDate
+            }
+            let endDate = rightMost[1]
+            if (endDate.length > 0) {
+                if (endDate.length <= 8) endDate += "000000"
+                filter.endValueDate = endDate
+            }
+        }
+        return filter
+    }
+    let monthsToBirthDate = (months) => {
+        let birthDate = new Date()
+        birthDate.setMonth(new Date().getMonth() - months)
+        var y = birthDate.getFullYear();
+        var m = birthDate.getMonth() + 1;
+        var d = birthDate.getDate();
+        let formattedDate = '' + y + (m < 10 ? '0' : '') + m + (d < 10 ? '0' : '') + d;
+        return formattedDate
+        //let birthDateEpoch = parseInt(birthDate.getTime()/1000)
+    }
+
+    let heFilter = (left, right) => {
+        let filter = {
+            '$type':'HealthElementByHcPartyTagCodeFilter',
+            'healthcarePartyId':hcpId
+        }
+        if (left.indexOf(':') === 0) {
+            filter.tagCode = right
+            filter.tagType = left.substr(1)
+        } else {
+            filter.codeCode = right
+            filter.codeType = left
+        }
+        return filter
     }
 }
 OrExpression
@@ -19,59 +57,77 @@ OrExpression
       tail:(_ "|" _ AndExpression _)* _
       { return tail.length && {'$type':'UnionFilter', 'filters':[head].concat(tail.map(it=>it[3]))} || head }
 AndExpression
-    = head:Expression
-      tail:(_ "&" _ Expression _)*
+    = head:Subtract
+      tail:(_ "&" _ Subtract _)*
       { return tail.length && {'$type':'IntersectionFilter', 'filters':[head].concat(tail.map(it=>it[3]))} || head }
+
+Subtract
+    = left:Expression
+      right:(_ "-" _ Expression _)*
+    {
+        return right.length && {'$type':'request', 'entity':'SUBTRACT', 'left':left, 'right':right[0][3]} || left
+    }
 
 Expression
 	= Request / ComparisonExpression
 
 Request
-	= entity:STR _ "[" op:OrExpression "]" {
-    	return {'$type':'request', 'entity':entity, 'filter':op }
+	= neg:"!"? _ entity:STR _ "[" op:OrExpression "]" {
+    	let f = {'$type':'request', 'entity':entity, 'filter':op }
+        return neg && {'$type':'ComplementFilter', 'subSet':f} || f;
     }
 
 ComparisonExpression
     = neg:"!"? _ "(" _ op:OrExpression _ ")" {
-    return neg && {'$type':'ComplementFilter', 'subFilter':op} || op;
+    return neg && {'$type':'ComplementFilter', 'subSet':op} || op;
 }
-/ left:Operand _ op:Comparison _ right:Operand {
+/ left:Operand _ op:Comparison _ right:Operand rightMost:OperandSuffix {
 	switch(op) {
     	case '==':
-        	return svcFilter(left,right)
+        	return svcFilter(left, right, rightMost)
         case '!=':
         	return {
-		      '$type':'ComplementFilter', 'subFilter': svcFilter(left,right)
+		      '$type':'ComplementFilter', 'subSet': svcFilter(left, right, rightMost)
             }
         case '<':
         case '>':
         	switch(left) {
             	case 'age':
-                	let birthDate = new Date()
-                    birthDate.setMonth(new Date().getMonth() - right)
-                    var y = birthDate.getFullYear();
-                    var m = birthDate.getMonth() + 1;
-                    var d = birthDate.getDate();
-    				let formattedDate = '' + y + (m < 10 ? '0' : '') + m + (d < 10 ? '0' : '') + d;
-                	let birthDateEpoch = parseInt(birthDate.getTime()/1000)
+                	let formattedDate = monthsToBirthDate(right)
                 	return Object.assign({
                     	'$type':'PatientByHcPartyDateOfBirthBetweenFilter',
                         'healthcarePartyId':hcpId,
-                    }, op === '>' ? {minDateOfBirth:0, maxDateOfBirth:formattedDate} : {minDateOfBirth:formattedDate})
-            }
+                    }, op === '>' ? {minDateOfBirth:0, maxDateOfBirth:formattedDate} : {minDateOfBirth:formattedDate, maxDateOfBirth:99990101})
+            } // TODO we can't omit min and max DateOfBirth
     }
 }
 
-Operand = QSTR / STR / AGE
+OperandSuffix
+    = "{" _ left:OptionalNUM _ "->" _ right:OptionalNUM _ "}" {
+        return [left, right]
+    } 
+    / "{" _ "<" months:AGE _ "}" {
+        return [monthsToBirthDate(months), ""]
+    }
+    /  "{" _ ">" months:AGE _ "}" {
+        return ["", monthsToBirthDate(months)]
+    }
+    / ""
+
+OptionalNUM = NUM / _
+
+Operand = QSTR / STR / AGE / NUM
 Comparison
-    = "==" / "!=" / ">"
+    = "==" / "!=" / ">" / "<" / "->"
 AGE
 	= value:[0-9]+ unit:[ym] {
     	return unit === 'y' ? Number(value.join(''))*12 : Number(value.join(''))
 
     }
+NUM
+    = value:[0-9]+ { return value.join('') }
 STR
-    = head:[:_a-zA-Z-] tail:[_a-zA-Z0-9-]* { return head + (tail && tail.join('') || '')}
+    = head:[:_a-zA-Z-] tail:[_a-zA-Z0-9-\.]* { return head + (tail && tail.join('') || '')}
 QSTR
     = "\"" str:[^"]* "\"" { return str.join('') }
 _ "whitespace"
--- src/filters.ts
+++ src/filters.ts
@@ -2,7 +2,7 @@
 // npm link /Users/simon/Documents/taktik/icc-api
 import * as WebCrypto from "node-webcrypto-ossl"
 import fetch from "node-fetch"
-import {iccContactApi, IccCryptoXApi, IccHcpartyXApi, iccPatientApi, ServiceDto} from 'icc-api'
+import {iccContactApi, IccCryptoXApi, IccHcpartyXApi, iccPatientApi, ServiceDto, iccHelementApi, HealthElementDto} from 'icc-api'
 import {flatMap, isEqual, omit} from 'lodash'
 import * as Peg from 'pegjs'
 
@@ -29,6 +29,8 @@ const patienticc = new iccPatientApi(host, headers, fetch)
 const cryptoicc = new IccCryptoXApi(host, headers, hcpartyicc, patienticc, new WebCrypto())
 // @ts-ignore
 const contacticc = new iccContactApi(host, headers, fetch)
+// @ts-ignore
+const helementicc = new iccHelementApi(host, headers, fetch)
 
 const hcpartyId = "782f1bcd-9f3f-408a-af1b-cd9f3f908a98"
 const privateKey = '308204bd020100300d06092a864886f70d0101010500048204a7308204a302010002820101009ad2f63357da7bb9b67b235b50f66c4968b391ba3340c4c4a697d0495512bff35f3692ffdcc867fa0602819d9fe9353f049b6c69e2dbf4a987e4d1b88b9475307c41427b33af8c0a6779a8347122f032cb801b54e2ce54e2edef2b1ae1f440a797945a4d0ab541711866ea32d096fe2da943bdd8251345fd8f50b0481e88f52e326a2cc9446d125c9643650182dbebf0272da6004a954acc21f8f47236fa7d3bbb256fb932aceb9b0fe081af27a3b476d0885905526b0e5faaa7d712536b77b05ff29a36b617a17ef611b8876346cc9ff864a295cc9ec2d5fe0efb0d94d99e5db96ea36a96d95ec63de639d243c74c773a4c350236265f71260de0fcd5fbb94b02030100010282010100878dd589b68dd06e155b52e58cc9749e0151d77193964db16fbad3dea0e1bdb633d2f0799cb0ca7899f26fd1b644d51dcbc6d8f10c73508f6e2fe57f129674d472b620a305e9d94ef2b20d977cc6fe4f3ae57b08a35bcbeeb42c072d8e4ff09bcb975448c7eb52d4d66ca4f8c0b0b2f2ff94140fbec6552d5fe161b683259ea3e95278ac83826f0674a4b0b5b6c717087abce79c73c9f6bf7832ef7337d8b07912244c30e3dc59512b8d2ab0fec288d8e561e29985e7eaaaa1e010a52ed025f5fa201c893214a42d9b17eff87752902063a1accc4eb169cd408aec4ee95e588e0bf5fccb6e945e67b965c6fb5d936c1b8cbf5e6dd6f7a9b8b4dd25f68ffcb68102818100ddc101d385681b81f527edb6dce5cb7ca9e2e7cb28fa1187933628bfbc38e9c153cd3783453a7e0ffbd2ad28ef67e879e08744d7148e83b3cb3fb7282ac03feed5d44cb7e70d014b1aef213d0c057d3d6c75653739ee22ba794c0a5f6194db84c6df3e0dddbdf57b1cc114881015f49c26eda572470dc708d2a1abc4c541671b02818100b2bbe5ab2f5d41323c8c9a6b65daf0771f416abc6c8c6b08a2bcd632e6ebba0d9efb6d99b435a3ae5b1b2b3ef450648e361bc6c480902d25b459ad120c05286ab7f91e24ecc8516ba9db086e8dabf5bb4ba97ef1c4c20a751841e472a41132145623eca0ca4fbb3025b4fb7430e0e5258afedb5017c2a0dd66cb8bcf0d172991028180345bc8049b71335d81f70587b1ac88594cfb88634daf8dc807183892dcec4b351c864ddf2ecf5ac8875afd0bb74b3f76d76ed8f037a856ac7306fe45fba21cf65582a5029f09510edcb32d93ee6cb55f75665a99a991f29d38da9d705be7fbd4e3e7fe0ce4186007cb884342c5198a01fca70bf3699775313e1a722629b5019502818006e5ab5234ccb3745dd3cb2db3cb841604b5c089aee2a84ab804f37b19602558db36b69f04ce4117bc5a4b0beddfa051c092c7d3d3663ce7c492e553d9f4e4ff614412beb8086ee3e9b51319390c56ba388c3ce2d585eb6363613f9090f63ce97dfd7ae725877820be83c264547289452e9cf117a123189412a06e2fba40979102818070faf47286b59425cb7c2f617f2b7b1b280b932f131a86b82e63c4fb240525ab40323ab902c507a4aee337f9f95b89aa9151d1ae2882bff497396e680407f5407ca154f20047017022eda8fe0438a473fb38123d36bc51bffc69e3c13fab4ecf16057529265e2c0993ca8886cc019c65e9460fe549b553fa48bb0f3ca0975e78'
@@ -49,24 +51,46 @@ async function rewriteFilter(filter: any, first: boolean, mainEntity: string = "
 					const patientIds: string[] = await servicesToPatientIds(servicesOutput)
 					return {$type: "PatientByIdsFilter", ids: patientIds}
 				}
+			} else if (filter.entity === "HE") {
+				const body = {filter: filter.filter}
+				const helementOutput = await helementicc.filterBy(body)
+				if (mainEntity === "PAT") {
+					console.log('AAAAA')
+					console.log(body)
+					console.log(helementOutput)
+					return {}
+					//const patientIds: string[] = await helementsToPatientIds(helementOutput)
+					//return {$type: "PatientByIdsFilter", ids: patientIds}
+				}
+			}
+			if (filter.entity === "SUBTRACT") {
+				const left = await rewriteFilter(filter.left, first, mainEntity)
+				const right = await rewriteFilter(filter.right, first, mainEntity)
+				return {$type: "ComplementFilter", superSet: left, subSet: right}
 			}
 			console.error("Filter not supported yet: " + filter)
 			return Promise.reject()
 		} else if (filter.$type !== "request") {
-			if (filter.filters) { // TODO also filter.filter or other?
-				let target = Object.assign({}, filter)
-				target.filters = await Promise.all(filter.filters.map(async (f: any) => await rewriteFilter(f, first, mainEntity))) // TODO cleaner way ?
+			if (filter.filters) {
+				let target = JSON.parse(JSON.stringify(filter))
+				target.filters = await Promise.all(filter.filters.map(async (f: any) => await rewriteFilter(f, first, mainEntity)))
 				return target
-			} else {
+			} else if (filter.subSet || filter.superSet) {
+				let target = JSON.parse(JSON.stringify(filter))
+				if (filter.subSet) target.subSet = await rewriteFilter(target.subSet, first, mainEntity)
+				if (filter.superSet) target.superSet = await rewriteFilter(target.superSet, first, mainEntity)
+				console.log(target)
+				return target
+			} else { // TODO maybe other conditions here
 				return filter
 			}
 		} else { // never hits this
 			console.error("Failed to parse filter: " + JSON.stringify(filter))
 			return Promise.reject()
 		}
-	} catch (e) {
-		console.error('Error occurred while rewriting filter: ')
-		console.error(e)
+	} catch (error) {
+		console.error('Error occurred while rewriting filter: ' + JSON.stringify(filter))
+		console.error(error)
 		return Promise.reject()
 	}
 }
@@ -74,14 +98,13 @@ async function rewriteFilter(filter: any, first: boolean, mainEntity: string = "
 async function handleFinalRequest(filter: any): Promise<any> {
 	if (filter.$type === "request" && filter.entity && filter.filter) {
 		if (filter.entity == 'PAT') {
-			//console.log('Final filter: ' + JSON.stringify(filter.filter))
 			return await patienticc.filterBy(undefined, undefined, undefined, undefined, undefined, undefined, {filter: filter.filter})
 		} else {
 			console.error("Entity not supported yet: " + filter.entity)
 			return Promise.reject()
 		}
 	} else {
-		console.error('Filter not valid: ' + filter)
+		console.error('Filter not valid: ' + JSON.stringify(filter))
 		return {}
 	}
 }
@@ -100,9 +123,31 @@ async function servicesToPatientIds(servicesOutput: any): Promise<string[]> {
 }
 
 async function run(): Promise<boolean> {
-	if (!(await tests())) return false
-
+	//if (!(await tests())) return false
+	await tests()
 	try {
+		// TODO labresult instead of diagnosis,
+		//const input = "PAT[(age>45y & SVC[ICPC == T89 & :CD-ITEM == diagnosis]) - SVC[LOINC == Hba1c & :CD-ITEM == diagnosis]]"
+		const input = "PAT[age>45y & SVC[ICPC == T89{<1m} & :CD-ITEM == diagnosis | ICPC == T90] - SVC[ICPC == T90]]"
+		const parsedInput = parser.parse(input)
+		//console.log('ParsedInput: ' + JSON.stringify(parsedInput))
+		const output = await rewriteFilter(parsedInput, true, "")
+		//console.log('Rewritten filter: ' + JSON.stringify(output))
+		const finalResult = await handleFinalRequest(output)
+		//console.log(finalResult)
+		console.log(finalResult.totalSize)
+
+		// femme de 25-65 sans cancer du col, sans procédure 002 ou 003 faite ou refusée(?) dans les 3 dernières années
+		//const input2 = "PAT[age>25y & age<65y - (SVC[CISP == X75{<3y19000101 -> 20200101} & :CD-ITEM == diagnosis] | HE[CISP == X75]) - SVC[CISP == X37.002] - SVC[CISP == X37.003]]"
+		const input2 = "PAT[age>25y & age<65y - SVC[CISP == X75{19000101 -> 20200101} & :CD-ITEM == diagnosis] - SVC[CISP == X37.002] - SVC[CISP == X37.003]]"
+		const parsedInput2 = parser.parse(input2)
+		console.log('-> ParsedInput: ' + JSON.stringify(parsedInput2))
+		const output2 = await rewriteFilter(parsedInput2, true, "")
+		console.log('-> Rewritten filter: ' + JSON.stringify(output2))
+		const finalResult2 = await handleFinalRequest(output2)
+		//console.log(finalResult2)
+		console.log(finalResult2.totalSize)
+
 		//console.log(JSON.stringify(parsedInput))
 		//const test = await servicesToPatientIds(servicesOutput)
 		//console.log('PatientIds: ' + JSON.stringify(test))
@@ -151,13 +196,14 @@ async function tests(): Promise<boolean> {
 							{
 								"$type": "ServiceByHcPartyTagCodeDateFilter",
 								"healthcarePartyId": "782f1bcd-9f3f-408a-af1b-cd9f3f908a98",
-								"codeCode": "T89",
-								"codeType": "ICPC"
+								"codeCode": "T89", // D6Aqqch
+								"codeType": "ICPC" // "LOINC"
+								//"dateSTart" // à faire
 							},
 							{
 								"$type": "ServiceByHcPartyTagCodeDateFilter",
 								"healthcarePartyId": "782f1bcd-9f3f-408a-af1b-cd9f3f908a98",
-								"tagCode": "diagnosis",
+								"tagCode": "diagnosis", // "labresult"
 								"tagType": "CD-ITEM"
 							}
 						]
@@ -166,6 +212,8 @@ async function tests(): Promise<boolean> {
 			]
 		}
 	}
+	// TODO SVC means "at least one exists", i could extend it to "SVC[...] > 3"
+	// TODO !SVC
 	const input = "PAT[age>45y & SVC[ICPC == T89 & :CD-ITEM == diagnosis]]"
 	const parsedInput = parser.parse(input)
 
@@ -173,13 +221,17 @@ async function tests(): Promise<boolean> {
 	const passed = isEqual(omit(p, ['filter.filters[0].maxDateOfBirth']), omit(filter, ['filter.filters[0].maxDateOfBirth']))
 	if (!passed) {
 		console.error('PegJS test failed!')
+		console.log(p)
 		return false
 	}
 
 	const output = await rewriteFilter(parsedInput, true, "")
+	//console.log('Rewritten filter: ' + JSON.stringify(output))
 	const finalResult = await handleFinalRequest(output)
+	//console.log(finalResult)
 	if (finalResult.totalSize !== 3) {
 		console.error('Full test failed, totalSize=' + finalResult.totalSize + ' (should be 3)')
+		return false
 	}
 
 	return true
